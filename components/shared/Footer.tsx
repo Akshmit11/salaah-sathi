29b682dc95c81a697c7bc6e385fe47c4ec9a9d05
Akshmit <saxena.akshmit@gmail.com>
2024-06-09 16:12:33 +0530

    small design changes

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="border-t border-r-0 border-l-0 border-b-0">
-      <div className="space-y-4 md:space-y-0 md:space-x-3 md:flex md:justify-between py-6 px-10 max-w-7xl lg:mx-auto w-full items-center">
+      <div className="space-y-4 md:space-y-0 md:space-x-3 md:flex md:justify-between py-6 px-5 sm:px-10 max-w-7xl lg:mx-auto w-full items-center">
         <Link href="/">
           <Image src={"/logo.png"} alt="logo" width={150} height={150} />
         </Link>
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -18,17 +18,20 @@ import { headerLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 
 const MobileNav = () => {
+  const [openSheet, setOpenSheet] = useState(false);
 
   const pathname = usePathname(); 
 
   return (
     <nav className="lg:hidden">
-      <Sheet>
+      <Sheet open={openSheet} onOpenChange={setOpenSheet}> 
         <SheetTrigger className="align-middle">
           <MenuIcon />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white lg:hidden">
-          <Image src={"/logo.svg"} alt="logo" width={128} height={38} />
+          <Link href={'/'} onClick={() => {setOpenSheet(false)}}> 
+            <Image src={"/logo.svg"} alt="logo" width={128} height={38} />
+          </Link>
           <Separator className="border border-gray-50" />
           <ul className="space-y-4">
             {headerLinks.map((link) => {
@@ -43,6 +46,7 @@ const MobileNav = () => {
                 >
                   <Link
                     href={link.route}
+                    onClick={() => {setOpenSheet(false)}}
                     className="hover:underline hover:underline-offset-2 font-medium text-sm"
                   >
                     {link.label}
@@ -54,7 +58,7 @@ const MobileNav = () => {
           <Separator className="border border-gray-50" />
           <div className="md:hidden w-fit">
             <Button asChild className="flex md:hidden gap-2 text-white">
-              <Link href={"/problems/upload"}>
+              <Link href={"/problems/upload"} onClick={() => {setOpenSheet(false)}}> 
                 <Plus className="w-5 h-5" color="white" />
                 Share Your Problem
               </Link>
