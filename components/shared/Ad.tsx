4af148b857f64559a89207f6289a55fdfa4e253b
Akshmit <saxena.akshmit@gmail.com>
2024-06-09 08:35:06 +0530

    getting upto date

--- components/shared/Ad.tsx
+++ components/shared/Ad.tsx
@@ -1,9 +1,9 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Ad = () => {
   return (
-    <section className='mb-4 w-full border-[1px] rounded-xl h-60 flex text-center items-center justify-center'>
-        Sponsored Advertisement
+    <section className="mb-4 w-full border-[1px] rounded-xl h-[220px] flex text-center items-center justify-center bg-[url('/images/sample9.png')] bg-cover bg-center bg-no-repeat bg-black">
     </section>
   )
 }
