4ebc65c9588ad16f07865894a0ebc7760a925130
Akshmit <saxena.akshmit@gmail.com>
2024-06-10 14:52:22 +0530

    btn added on mobile view

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,11 @@ import Ad from "@/components/shared/Ad";
 import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import SearchComponent from "@/components/shared/SearchComponent";
+import { Button } from "@/components/ui/button";
 import { getAllProblems } from "@/lib/actions/problem.actions";
 import { SearchParamProps } from "@/types";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home({ searchParams }: SearchParamProps) {
 
@@ -22,6 +25,12 @@ export default async function Home({ searchParams }: SearchParamProps) {
     <main className="px-4 sm:px-0">
       <Ad />
       <div className="w-full mb-4 flex flex-col lg:flex-row items-center">
+        <Button asChild className="mb-4 w-full md:hidden gap-2 text-white">
+          <Link href={"/problems/upload"}>
+            <Plus className="w-5 h-5" color="white" />
+            Share Your Problem
+          </Link>
+        </Button>
         <SearchComponent /> 
         <CategoryFilter />
       </div>
